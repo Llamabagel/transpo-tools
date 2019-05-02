4df5b34e950449dc06507f6a793c083b6ef0ef53
derek <derek@llamabagel.ca>
2019-05-02 10:35:09 -0400

    Remove obsolete configuration check code

--- package-data/src/main/kotlin/ca/llamabagel/transpo/tools/ConfigureCommand.kt
+++ package-data/src/main/kotlin/ca/llamabagel/transpo/tools/ConfigureCommand.kt
@@ -1,16 +1,7 @@
 package ca.llamabagel.transpo.tools
 
-import ca.llamabagel.transpo.tools.Configuration
 import com.github.ajalt.clikt.core.CliktCommand
 import org.flywaydb.core.Flyway
-import java.io.File
-import java.lang.Exception
-import java.nio.charset.StandardCharsets
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.sql.Connection
-import java.sql.DriverManager
-import java.util.*
 import kotlin.system.exitProcess
 
 class ConfigureCommand : CliktCommand(name = "configure") {
@@ -22,20 +13,6 @@ class ConfigureCommand : CliktCommand(name = "configure") {
         }
 
         flyway.migrate()
-    }
-
-    private fun validateConfig(config: Properties): Boolean {
-        fun validateProperty(property: String): Boolean {
-            return if (!config.containsKey(property)) {
-                println("$property not defined in config.properties")
-                false
-            } else true
-        }
-
-        return validateProperty("SQL_HOST") &&
-                validateProperty("SQL_PORT") &&
-                validateProperty("SQL_USER") &&
-                validateProperty("SQL_PASSWORD")
     }
 
     private fun getDbConnection(): Flyway? {
